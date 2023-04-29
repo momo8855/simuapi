feb1ca8614f8dc703f25a64bc9bc529c935a66ea
momo8855 <116127683+momo8855@users.noreply.github.com>
2023-04-29 04:37:49 +0200

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -2,36 +2,36 @@ import streamlit as st
 import pandas as pd
 
 def assign_range(row, df):
-    prev_prob = 0 if row.name == 0 else df.loc[row.name-1, 'cumulative_probability']
+    prev_prob = 0 if row.name == 0 else df.loc[row.name-1, 'cumu_prob']
     start = int(round(prev_prob, 2)*100 ) + 1
-    end = int(round(row['cumulative_probability'], 2) * 100)
+    end = int(round(row['cumu_prob'], 2) * 100)
     return f"{start:02d}-{end:02d}"
 
 def simulate_queue(customers, inter_arrival_times, inter_arrival_probs, service_times, service_probs, random_arrival, random_service):
     df_time = pd.DataFrame({
-    'inter_arrival_time': inter_arrival_times,
-    'probability': inter_arrival_probs
+    'arrival_time': inter_arrival_times,
+    'prob': inter_arrival_probs
     })
 
     # Calculate the cumulative probability distribution
-    df_time['cumulative_probability'] = df_time['probability'].cumsum()
+    df_time['cumu_prob'] = df_time['prob'].cumsum()
 
     # Add the random digit assignment column
-    df_time['random_digit_assignment'] = df_time.apply(assign_range, args=(df_time,), axis=1)
+    df_time['random_ranges'] = df_time.apply(assign_range, args=(df_time,), axis=1)
 
 
 
     #construct table 2
     df_service = pd.DataFrame({
         'service_time': service_times,
-        'probability': service_probs
+        'prob': service_probs
     })
 
     # Calculate the cumulative probability distribution
-    df_service['cumulative_probability'] = df_service['probability'].cumsum()
+    df_service['cumu_prob'] = df_service['prob'].cumsum()
 
     # Add the random digit assignment column
-    df_service['random_digit_assignment'] = df_service.apply(assign_range, args=(df_service,), axis=1)
+    df_service['random_ranges'] = df_service.apply(assign_range, args=(df_service,), axis=1)
 
     table = []
     EndS = 0
@@ -46,20 +46,20 @@ def simulate_queue(customers, inter_arrival_times, inter_arrival_probs, service_
 
         rowArray.append(int(row + 1))#Customer
         #RN_interval = int(input("Enter RN interval(t) for Customer "+str(row + 1)+": "))
-        RN_interval = random_arrival[row]
+        RN_interval = random_arrival[row] if row != 0 else '----'
         rowArray.append(RN_interval)#RN interval(t)
         #intervalValue = int(input("Enter interval(t) for Customer "+str(row + 1)+": "))
-        match_row_time = df_time[df_time['random_digit_assignment'].apply(lambda x: int(x.split('-')[0]) <= int(RN_interval) <= int(x.split('-')[1]))]
-        if len(match_row_time) == 0:
-            intervalValue = 0
-        else:
-            intervalValue = match_row_time['inter_arrival_time'].iloc[0]
+        match_row_time = df_time[df_time['random_ranges'].apply(lambda x: int(x.split('-')[0]) <= int(RN_interval) <= int(x.split('-')[1]))] if row != 0 else None
+        
+        intervalValue = 0 if row == 0 else match_row_time['arrival_time'].iloc[0]
+
+            
         rowArray.append(intervalValue)#interval(t)
         Arrival += intervalValue
         rowArray.append(Arrival)#Arrival(t)
 
         rowArray.append(random_service[row])#RN Service(t)
-        match_row_service = df_service[df_service['random_digit_assignment'].apply(lambda x: int(x.split('-')[0]) <= int(round(random_service[row], 2)) <= int(x.split('-')[1]))]
+        match_row_service = df_service[df_service['random_ranges'].apply(lambda x: int(x.split('-')[0]) <= int(round(random_service[row], 2)) <= int(x.split('-')[1]))]
         service = match_row_service['service_time'].iloc[0]
         #service = int(input("Enter Service(t) for Customer "+str(row+1)+": "))
         rowArray.append(service)#Service
@@ -124,17 +124,35 @@ if st.button('Submit'):
     inter_arrival_times = [int(num) for num in input1.split(', ')]
     inter_arrival_probs = [float(num) for num in input2.split(', ')]
     random_arrival = [int(num) for num in input3.split(', ')]
+    random_arrival.insert(0, 0)
     service_times = [int(num) for num in input4.split(', ')]
     service_probs = [float(num) for num in input5.split(', ')]
     random_service = [int(num) for num in input6.split(', ')]
 
     df_1, df_2, df_3 = simulate_queue(customers = costomers, inter_arrival_times = inter_arrival_times, inter_arrival_probs = inter_arrival_probs, 
                                   service_times = service_times, service_probs = service_probs, random_arrival = random_arrival, random_service = random_service)
-    
+    # Use streamlit_columns to create two columns
+    col1, col2 = st.columns(2)
+
+    df_2 = df_2.set_index('arrival_time')
+    df_3 = df_3.set_index('service_time')
+
+    # Display the dataframes in each column
+    with col1:
+        st.write('Table 1')
+        st.write(df_2)
+
+    with col2:
+        st.write('Table 2')
+        st.write(df_3)
+
+
     df_1 = df_1.set_index('Customer')
+    st.write('Simulation Table')
     st.write(df_1)
 
     chart_data = df_1['Wait(t)']
+    st.write('Simulation Chart')
     st.bar_chart(chart_data)
 
 
